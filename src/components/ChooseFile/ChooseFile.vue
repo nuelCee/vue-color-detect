be77699ed8f6ef0c5b41e8eaa7b7862034ca760e
Nwosu Chinaecherem Emmanuel <54024186+nuelCee@users.noreply.github.com>
2020-12-26 23:52:50 +0100

    UI: Image Select Complete

---
+++ src/components/ChooseFile/ChooseFile.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="choose">
+    <div class="choose__con">
+      <div class="choose__con-action">
+        <div class="choose__con-action--browse">
+          <label for="image">Select file</label>
+          <input type="file" id="image" @change="preview($event)" />
+        </div>
+      </div>
+      <img :src="placeholder" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChooseFile',
+  data() {
+    return {
+      placeholder:
+        'https://images.unsplash.com/photo-1504568758572-1c648768a420?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
+    };
+  },
+  props: {
+    msg: String,
+  },
+  methods: {
+    preview(e) {
+      const reader = new FileReader();
+      const file = e.target.files;
+      reader.onload = () => {
+        this.placeholder = reader.result;
+      };
+
+      console.log(file);
+      reader.readAsDataURL(file);
+
+      // console.log(e);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import "./style/choosefile.scss";
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <choose-file msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import ChooseFile from '../components/ChooseFile/ChooseFile.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    ChooseFile,
+  },
+};
+</script>
